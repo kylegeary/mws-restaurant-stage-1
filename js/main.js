04e1e33d162898670865731e4e2597835ee24bce
Kyle Geary <Kgeary09@gmail.com>
2018-10-31 23:54:42 -0500

    add serviceworkers and update a11y

--- js/main.js
+++ js/main.js
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex='3';
   li.append(more)
 
   return li
@@ -198,6 +199,68 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/sw.js')
+  .catch(function(err){
+    console.error(err);
+  });
+}
+
+console.log('Service Worker: Registered');
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      if (response) {
+        console.log("found", e.request, ' in cache');
+        return response;
+      } else {
+        console.loge('Could not find ', e.request, ' in cache, Fetching.');
+        return fetch(e.request)
+          .then(function (response) {
+            const clonedResponse = response.clone();
+            caches.open('V1').then(function (cache) {
+              cache.put(e.request, response);
+            })
+            return response;
+          })
+          .catch(function (err) {
+            console.error(err);
+          });
+      }
+    })
+  );
+});
+
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+]
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
